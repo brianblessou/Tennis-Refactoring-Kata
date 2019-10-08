0938d9c08830ca19bc7684332a8fa554b576e369
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-08 16:08:09 +0200

    refactoring small functions

--- python/tennis_game.py
+++ python/tennis_game.py
@@ -13,26 +13,40 @@ class TennisGame1:
             self.p1points += 1
         else:
             self.p2points += 1
-    
+
+    def calculate_equal_score(self):
+        """
+        This function calculates the scores when they are equal.
+        :return: a string containing the score.
+        """
+        return {
+            0 : "Love-All",
+            1 : "Fifteen-All",
+            2 : "Thirty-All",
+        }.get(self.p1points, "Deuce")
+
+    def calculate_gte_four(self):
+        """
+        This funciton calculates the score when they are bigger or equal than four.
+        :return: a string containing the score.
+        """
+        minusResult = self.p1points-self.p2points
+        if (minusResult==1):
+            return "Advantage " + self.player1Name
+        elif (minusResult ==-1):
+            return "Advantage " + self.player2Name
+        elif (minusResult>=2):
+            return "Win for " + self.player1Name
+        else:
+            return "Win for " + self.player2Name
+
     def score(self):
         result = ""
-        tempScore=0
+        tempScore = 0
         if (self.p1points==self.p2points):
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
+            result = self.calculate_equal_score()
         elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result ="Advantage " + self.player1Name
-            elif (minusResult ==-1):
-                result ="Advantage " + self.player2Name
-            elif (minusResult>=2):
-                result = "Win for " + self.player1Name
-            else:
-                result ="Win for " + self.player2Name
+            result = self.calculate_gte_four()
         else:
             for i in range(1,3):
                 if (i==1):
@@ -47,6 +61,7 @@ class TennisGame1:
                     3 : "Forty",
                 }[tempScore]
         return result
+
 
 
 
