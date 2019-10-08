e9495d59a89ca7ad646a4259568374c269d021b2
brianblessou <brian.blessou@gmail.com>
2019-10-08 17:46:58 +0200

    remove the loop for

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -16,7 +16,7 @@ class TennisGame1(val player1Name: String, val player2Name: String) extends Tenn
   /**
     * Calculate the score if the players have the score
     *
-    * @param m_score1, int which is the score of the first player
+    * @param m_score1 , int which is the score of the first player
     * @return a string which contains the score
     */
   def calculateEqualScore(m_score1: Int): String = {
@@ -37,8 +37,8 @@ class TennisGame1(val player1Name: String, val player2Name: String) extends Tenn
   def calculateGteFour(m_score1: Int, m_score2: Int): String = {
     val minusResult = m_score1 - m_score2
 
-    if (minusResult == 1)  s"Advantage ${player1Name}"
-    else if (minusResult == -1)  s"Advantage ${player2Name}"
+    if (minusResult == 1) s"Advantage ${player1Name}"
+    else if (minusResult == -1) s"Advantage ${player2Name}"
     else if (minusResult >= 2) s"Win for ${player1Name}"
     else s"Win for ${player2Name}"
   }
@@ -53,23 +53,20 @@ class TennisGame1(val player1Name: String, val player2Name: String) extends Tenn
       score = calculateGteFour(m_score1, m_score2)
     }
     else {
-      for (i <- 1 until 3 by 1) {
-        if (i == 1) tempScore = m_score1
-        else {
-          score += "-";
-          tempScore = m_score2;
-        }
-        val tempScore2 = tempScore match {
-          case 0 => "Love"
-          case 1 => "Fifteen"
-          case 2 => "Thirty"
-          case 3 => "Forty"
-        }
-        score += tempScore2
-      }
+      score = getScore(m_score1) + "-" + getScore(m_score2)
     }
 
     return score
   }
 
+
+  def getScore(tempScore: Int): String = {
+    tempScore match {
+      case 0 => "Love"
+      case 1 => "Fifteen"
+      case 2 => "Thirty"
+      case 3 => "Forty"
+    }
   }
+
+}
