4a65a6aed46ba5d9ea19189eba48ce351a17f75b
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-09 14:19:50 +0200

    added smaller functions

--- python/src/tennis_game3.py
+++ python/src/tennis_game3.py
@@ -23,10 +23,27 @@ class TennisGame3:
         else:
             if self.player_one_score == self.player_two_score:
                 return "Deuce"
-            if self.player_one_score > self.player_two_score:
-                winning_player = self.player_one_name
-            else:
-                winning_player = self.player_two_name
-            if (self.player_one_score - self.player_two_score) * (self.player_one_score - self.player_two_score) == 1:
-                    return "Advantage " + winning_player
-            else: return "Win for " + winning_player
+            winning_player = self.get_winning_player()
+            return self.parse_advantage_or_win(winning_player)
+
+    def parse_advantage_or_win(self,winning_player):
+        """
+        Helper function to parse get the output score if the game is at a stage that is
+        either an advantage or somebody will win.
+        :param winning_player: player with the advantage.
+        :return: output result.
+        """
+        if (self.player_one_score - self.player_two_score) * (self.player_one_score - self.player_two_score) == 1:
+            return "Advantage " + winning_player
+        else:
+            return "Win for " + winning_player
+
+    def get_winning_player(self):
+        """
+        Helper funciton to calculate the player who has an advantage or won.
+        :return: player with the highest score.
+        """
+        if self.player_one_score > self.player_two_score:
+            return self.player_one_name
+        else:
+            return self.player_two_name
