88b64c9e21bcb1a1ab051d21d77e059b634cf1cb
brianblessou <brian.blessou@gmail.com>
2019-10-09 11:29:59 +0200

    replace if condition by match case

--- scala/src/main/scala/tennis/game2/TennisGame2.scala
+++ scala/src/main/scala/tennis/game2/TennisGame2.scala
@@ -2,130 +2,116 @@ package tennis.game2
 
 import tennis.TennisGame
 
-class TennisGame2 (val player1Name : String, val player2Name : String) extends TennisGame {
+class TennisGame2(val player1Name: String, val player2Name: String) extends TennisGame {
 
-    var P1point = 0
-    var P2point  = 0
+  var P1point = 0
+  var P2point = 0
 
-    var P1res = ""
-    var P2res = ""
+  var P1res = ""
+  var P2res = ""
 
-    def calculateScore() : String = {
-        var score = ""
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love"
-            if (P1point==1)
-                score = "Fifteen"
-            if (P1point==2)
-                score = "Thirty"
-            score += "-All"
-        }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce"
+  def getConcatenatedScore(P1res: String, P2res: String): String = {
+    P1res + "-" + P2res
+  }
 
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen"
-            if (P1point==2)
-                P1res = "Thirty"
-            if (P1point==3)
-                P1res = "Forty"
+  def calculateScore(): String = {
+    var score = ""
 
-            P2res = "Love"
-            score = P1res + "-" + P2res
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen"
-            if (P2point==2)
-                P2res = "Thirty"
-            if (P2point==3)
-                P2res = "Forty"
+    if (P1point == P2point && P1point < 4) {
+      P1point match {
+        case 0 => score = "Love"
+        case 1 => score = "Fifteen"
+        case 2 => score = "Thirty"
+        case _ => print("Exception")
+      }
 
-            P1res = "Love"
-            score = P1res + "-" + P2res
-        }
-
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty"
-            if (P1point==3)
-                P1res="Forty"
-            if (P2point==1)
-                P2res="Fifteen"
-            if (P2point==2)
-                P2res="Thirty"
-            score = P1res + "-" + P2res
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty"
-            if (P2point==3)
-                P2res="Forty"
-            if (P1point==1)
-                P1res="Fifteen"
-            if (P1point==2)
-                P1res="Thirty"
-            score = P1res + "-" + P2res
-        }
-
-        if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1"
-        }
-
-        if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2"
-        }
-
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1"
-        }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2"
-        }
-        return score
+      score += "-All"
     }
 
-    def SetP1Score(number : Int) {
+    if (P1point == P2point && P1point >= 3)
+      score = "Deuce"
 
-        for (i <- 0 until number by 1)
-        {
-            P1Score()
-        }
+    if (P1point > 0 && P2point == 0) {
 
+      P1point match {
+        case 1 => P1res = "Fifteen"
+        case 2 => P1res = "Thirty"
+        case 3 => P1res = "Forty"
+        case _ => print("Exception")
+      }
+      P2res = "Love"
+
+      score = getConcatenatedScore(P1res, P2res)
     }
 
-    def SetP2Score(number : Int){
+    if (P2point > 0 && P1point == 0) {
 
-      for (i <- 0 until number by 1)
-        {
-            P2Score()
-        }
-
+      P2point match {
+        case 1 => P2res = "Fifteen"
+        case 2 => P2res = "Thirty"
+        case 3 => P2res = "Forty"
+        case _ => print("Exception")
+      }
+      P1res = "Love"
+      score = getConcatenatedScore(P1res, P2res)
     }
 
-    def P1Score(){
-        P1point += 1
+    if (P1point > P2point && P1point < 4) {
+      P1point match {
+        case 2 => P1res = "Thirty"
+        case 3 => P1res = "Forty"
+        case _ => print("Exception")
+      }
+
+      P2point match {
+        case 1 => P2res = "Fifteen"
+        case 2 => P2res = "Thirty"
+        case _ => print("Exception")
+      }
+
+
+      score = getConcatenatedScore(P1res, P2res)
+    }
+    if (P2point > P1point && P2point < 4) {
+
+      P1point match {
+        case 1 => P1res = "Fifteen"
+        case 2 => P1res = "Thirty"
+        case _ => print("Exception")
+      }
+
+      P2point match {
+        case 2 => P2res = "Thirty"
+        case 3 => P2res = "Forty"
+        case _ => print("Exception")
+      }
+
+      score = getConcatenatedScore(P1res, P2res)
     }
 
-    def P2Score(){
-        P2point +=1
+    if (P1point > P2point && P2point >= 3) {
+      score = "Advantage player1"
     }
 
-    def wonPoint(player : String) {
-        if (player == "player1")
-            P1Score()
-        else
-            P2Score()
+    if (P2point > P1point && P1point >= 3) {
+      score = "Advantage player2"
     }
+
+    if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+      score = "Win for player1"
+    }
+    if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+      score = "Win for player2"
+    }
+
+    return score
+  }
+
+
+  def wonPoint(player: String) {
+    if (player == "player1")
+      P1point += 1
+    else
+      P2point += 1
+  }
 }
---
+++ scala/src/test/scala/tennis/game2/TennisUnitTest.scala
@@ -0,0 +1,41 @@
+package tennis.game2
+
+import org.junit.Assert._
+import org.junit.Test
+
+
+/**
+  *  Unit tests
+  *
+  */
+class TennisUnitTest() {
+
+//  @Test
+//  def checkCalculateScoreForEquality(): Unit = {
+//    assertEquals("Love-All", Equality.calculateScore(0))
+//    assertEquals("Fifteen-All", Equality.calculateScore(1))
+//    assertEquals("Thirty-All", Equality.calculateScore(2))
+//    assertEquals("Deuce", Equality.calculateScore(3))
+//  }
+
+//  @Test
+//  def checkCalculateScoreForRegular(): Unit = {
+//    assertEquals("Love-All", Regular.calculateScore(0,0))
+//    assertEquals("Love-Fifteen", Regular.calculateScore(0,1))
+//    assertEquals("Love-Thirty", Regular.calculateScore(0,2))
+//    assertEquals("Love-Forty", Regular.calculateScore(0,3))
+//  }
+
+//
+//  @Test
+//  def checkCalculateScoreForMatchPoints(): Unit = {
+//    val game = new TennisGame1("player1", "player2")
+//    assertEquals("Advantage player1", game.calculateScoreForMatchPoints(3,2, "player1", "player2"))
+//    assertEquals("Advantage player2", game.calculateScoreForMatchPoints(2,3,"player1", "player2"))
+//    assertEquals("Win for player1", game.calculateScoreForMatchPoints(5,1,"player1", "player2"))
+//    assertEquals("Win for player2", game.calculateScoreForMatchPoints(4,4,"player1", "player2"))
+//  }
+
+}
+
+
