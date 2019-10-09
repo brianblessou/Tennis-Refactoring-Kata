eefcb128dc9b943466d898652831289cb5f5b776
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-09 11:30:03 +0200

    refactored the common code to a single method

--- python/src/tennis_game2.py
+++ python/src/tennis_game2.py
@@ -11,62 +11,31 @@ class TennisGame2:
         else:
             self.p2points +=1
 
+    def calculate_score(self, points):
+        if points == 0:
+            return "Love"
+        elif points == 1:
+            return "Fifteen"
+        elif points == 2:
+            return "Thirty"
+        else:
+            return "Forty"
+
+
     def score(self):
         result = ""
-        if (self.p1points == self.p2points and self.p1points < 3):
-            if (self.p1points==0):
-                result = "Love"
-            if (self.p1points==1):
-                result = "Fifteen"
-            if (self.p1points==2):
-                result = "Thirty"
-            result += "-All"
-        if (self.p1points==self.p2points and self.p1points>2):
-            result = "Deuce"
+        if self.p1points == self.p2points:
+            if self.p1points < 3:
+                result = self.calculate_score(self.p1points) + "-All"
+            else:
+                result = "Deuce"
 
-        P1res = ""
-        P2res = ""
-        if (self.p1points > 0 and self.p2points==0):
-            if (self.p1points==1):
-                P1res = "Fifteen"
-            if (self.p1points==2):
-                P1res = "Thirty"
-            if (self.p1points==3):
-                P1res = "Forty"
-
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points==0):
-            if (self.p2points==1):
-                P2res = "Fifteen"
-            if (self.p2points==2):
-                P2res = "Thirty"
-            if (self.p2points==3):
-                P2res = "Forty"
-
-            P1res = "Love"
-            result = P1res + "-" + P2res
-
-
-        if (self.p1points>self.p2points and self.p1points < 4):
-            if (self.p1points==2):
-                P1res="Thirty"
-            if (self.p1points==3):
-                P1res="Forty"
-            if (self.p2points==1):
-                P2res="Fifteen"
-            if (self.p2points==2):
-                P2res="Thirty"
-            result = P1res + "-" + P2res
-        if (self.p2points>self.p1points and self.p2points < 4):
-            if (self.p2points==2):
-                P2res="Thirty"
-            if (self.p2points==3):
-                P2res="Forty"
-            if (self.p1points==1):
-                P1res="Fifteen"
-            if (self.p1points==2):
-                P1res="Thirty"
+        if (self.p2points < self.p1points < 4) \
+            or (self.p1points < self.p2points < 4) \
+            or (self.p2points > 0 and self.p1points==0) \
+            or (self.p1points > 0 and self.p2points==0):
+            P1res = self.calculate_score(self.p1points)
+            P2res = self.calculate_score(self.p2points)
             result = P1res + "-" + P2res
 
         if (self.p1points > self.p2points and self.p2points >= 3):
