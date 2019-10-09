2877ff779ad024dc79cb1b2aa9cc0651df61fa9e
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-09 11:10:47 +0200

    refactoring tennis game 2: removed redundant methods.

--- python/src/tennisgame1/equalitypoints.py
+++ python/src/tennisgame1/equalitypoints.py
@@ -6,7 +6,7 @@ class to calculate the outcome string when the scores are equal.
 class EqualityPoints(object):
 
     @staticmethod
-    def calculate_score_equality(score):
+    def calculateScore_equality(score):
         """
         This function calculates the scores when they are equal.
         :return: a string containing the score.
