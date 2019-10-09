2d7247a78617c0a21f50eb53109e8f4d65067cec
Adriaan Slechten <adriaan.slechten@businessdecision.be>
2019-10-09 14:06:45 +0200

    excercise 3: renamed variables

--- python/src/tennis_game3.py
+++ python/src/tennis_game3.py
@@ -1,23 +1,28 @@
 class TennisGame3:
-    def __init__(self, player1Name, player2Name):
-        self.p1N = player1Name
-        self.p2N = player2Name
-        self.p1 = 0
-        self.p2 = 0
+    def __init__(self, player_one_name, player_two_name):
+        self.player_one_name = player_one_name
+        self.player_two_name = player_two_name
+        self.player_one_score = 0
+        self.player_two_score = 0
 
-    def won_point(self, n):
-        if n == self.p1N:
-            self.p1 += 1
+    def won_point(self, current_player):
+        if current_player == self.player_one_name:
+            self.player_one_score += 1
         else:
-            self.p2 += 1
+            self.player_two_score += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
+        if (self.player_one_score < 4 and self.player_two_score < 4) \
+                and (self.player_one_score + self.player_two_score < 6):
+            result_scores = ["Love", "Fifteen", "Thirty", "Forty"]
+            result_string = result_scores[self.player_one_score]
+            if self.player_one_score == self.player_two_score:
+                return result_string + "-All"
+            else:
+                result_string + "-" + result_scores[self.player_two_score]
         else:
-            if (self.p1 == self.p2):
+            if self.player_one_score == self.player_two_score:
                 return "Deuce"
-            s = self.p1N if self.p1 > self.p2 else self.p2N
-            return "Advantage " + s if ((self.p1-self.p2)*(self.p1-self.p2) == 1) else "Win for " + s
+            result_string = self.player_one_name if self.player_one_score > self.player_two_score else self.player_two_name
+            return "Advantage " + result_string if ((self.player_one_score - self.player_two_score) * (self.player_one_score - self.player_two_score) == 1) \
+                else "Win for " + result_string
