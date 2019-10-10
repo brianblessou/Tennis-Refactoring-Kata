523f763931f53be671ce7d4f0d5f92da87e8faa2
brianblessou <brian.blessou@gmail.com>
2019-10-10 15:17:21 +0200

    Add missing javadoc

--- scala/src/main/scala/tennis/game2/TennisGame2.scala
+++ scala/src/main/scala/tennis/game2/TennisGame2.scala
@@ -2,6 +2,15 @@ package tennis.game2
 
 import tennis.TennisGame
 
+
+/**
+  * Class to play a tennis game between two players.
+  *
+  * It will calculate the score in case of equality, match points or regular game
+  *
+  * @param player1Name, a string
+  * @param player2Name, a string
+  */
 class TennisGame2(val player1Name: String, val player2Name: String) extends TennisGame {
 
   var P1point = 0
@@ -10,133 +19,106 @@ class TennisGame2(val player1Name: String, val player2Name: String) extends Tenn
   var P1res = ""
   var P2res = ""
 
-  def getConcatenatedScore(P1res: String, P2res: String): String = {
-    P1res + "-" + P2res
-  }
-
-
-  def calculScoreIfEquality(): String = {
-    var score: String = ""
-
-    if (P1point >= 3) {
-      score = "Deuce"
-    } else  {
-      P1point match {
-        case 0 => score = "Love"
-        case 1 => score = "Fifteen"
-        case 2 => score = "Thirty"
-        case _ => print("Exception")
-      }
-      score += "-All"
-    }
-
-    score
-  }
-
-
-  def calculateScoreIP1GtP2(): String = {
-    var score: String = ""
-
-    if (P1point > 0 && P2point == 0) {
-
-      P1point match {
-        case 1 => P1res = "Fifteen"
-        case 2 => P1res = "Thirty"
-        case 3 => P1res = "Forty"
-        case _ => print("Exception")
-      }
-      P2res = "Love"
-
-      score = getConcatenatedScore(P1res, P2res)
-    } else if (P1point > P2point && P1point < 4) {
-      P1point match {
-        case 2 => P1res = "Thirty"
-        case 3 => P1res = "Forty"
-        case _ => print("Exception")
-      }
-
-      P2point match {
-        case 1 => P2res = "Fifteen"
-        case 2 => P2res = "Thirty"
-        case _ => print("Exception")
-      }
-
-
-      score = getConcatenatedScore(P1res, P2res)
-    } else if (P1point > P2point && P2point >= 3) {
-      score = "Advantage player1"
-    }
-
-    score
-  }
-
-
-  def calculateScoreIP2GtP1(): String = {
-    var score: String = ""
-
-    if (P2point > 0 && P1point == 0) {
-
-      P2point match {
-        case 1 => P2res = "Fifteen"
-        case 2 => P2res = "Thirty"
-        case 3 => P2res = "Forty"
-        case _ => print("Exception")
-      }
-      P1res = "Love"
-      score = getConcatenatedScore(P1res, P2res)
-    } else if (P2point > P1point && P2point < 4) {
-
-      P1point match {
-        case 1 => P1res = "Fifteen"
-        case 2 => P1res = "Thirty"
-        case _ => print("Exception")
-      }
-
-      P2point match {
-        case 2 => P2res = "Thirty"
-        case 3 => P2res = "Forty"
-        case _ => print("Exception")
-      }
-
-      score = getConcatenatedScore(P1res, P2res)
-    } else {
-      if (P2point > P1point && P1point >= 3) {
-        score = "Advantage player2"
-      }
-    }
-
-    score
-  }
-
+  /**
+    * Main function to calculate the final score depending on the points of each player
+    *
+    * @return a result, the result of the game
+    */
   def calculateScore(): String = {
-    var score = ""
 
-    // In case of equality
-    if (P1point == P2point) {
-      score = calculScoreIfEquality()
+    if (matchIsFinished(P1point, P2point)) {
+      getWinner(player1Name, player2Name, P1point > P2point)
+    } else if (P1point == P2point) {
+      calculScoreIfEquality(P1point)
+    } else if (P1point > P2point) {
+      calculateScoeForDifferentScore(player1Name, P1point)
+    } else  {
+      calculateScoeForDifferentScore(player2Name, P2point)
     }
+  }
 
-    // IF P1point > P2Point
-    if (P1point > P2point) {
-      score = calculateScoreIP1GtP2()
+  /**
+    * Get the player who won the game
+    *
+    * @param player1Name a string
+    * @param player2Name a string
+    * @param player1Won a boolean, true if player1 won else false
+    * @return a string, the result of the game
+    */
+  def getWinner(player1Name: String, player2Name: String, player1Won: Boolean): String = {
+    if (player1Won) s"Win for ${player1Name}" else s"Win for ${player2Name}"
+  }
 
+  /**
+    * Calcul score in case of equality
+    *
+    * @param playerPoint an int
+    * @return a string, the score name
+    */
+  def calculScoreIfEquality(playerPoint: Int): String = {
+    playerPoint match {
+      case 0 => "Love-All"
+      case 1 => "Fifteen-All"
+      case 2 => "Thirty-All"
+      case _ => "Deuce"
     }
+  }
 
-    // IF P2point > P1point
-    if (P2point > P1point) {
-     score = calculateScoreIP2GtP1()
+
+  /**
+    * Calculate the result of a set if no equality
+    *
+    * @param playerName, a string
+    * @param playerPoint, an int
+    * @return a string, the nresult of the set
+    */
+  def calculateScoeForDifferentScore(playerName: String, playerPoint: Int): String = {
+    if (playerPoint == 0) {
+      getConcatenatedScore(getScore(P1point), getScore(P2point))
+    } else if (playerPoint < 4) {
+      getConcatenatedScore(getScore(P1point), getScore(P2point))
+    } else  {
+      s"Advantage ${playerName}"
     }
+  }
 
 
-    // Check for those ones
-    if (P1point >= 4 && (P1point - P2point) >= 2) {
-      score = "Win for player1"
+  /**
+    * Function to get the score name at the end of the set
+    *
+    * @param playerScore
+    * @return a string, the score name for a player
+    */
+  def getScore(playerScore: Int): String = {
+    playerScore match {
+      case 0 => "Love"
+      case 1 =>"Fifteen"
+      case 2 => "Thirty"
+      case 3 => "Forty"
     }
-    if (P2point >= 4 && (P2point - P1point) >= 2) {
-      score = "Win for player2"
-    }
+  }
 
-    return score
+  /**
+    * Test if the match is finished or not.
+    *
+    * @param player1Point an Int
+    * @param player2Point an Int
+    * @return a Boolean,
+    */
+  def matchIsFinished(player1Point: Int, player2Point: Int): Boolean = {
+    (player1Point >= 4 || player2Point >= 4) && Math.abs(player1Point - player2Point) >= 2
+  }
+
+
+  /**
+    * Concatenate the final score name for two players.
+    *
+    * @param player1Result a string
+    * @param player2Result a string
+    * @return a string, score concatenated
+    */
+  def getConcatenatedScore(player1Result: String, player2Result: String): String = {
+    player1Result + "-" + player2Result
   }
 
 
@@ -146,4 +128,5 @@ class TennisGame2(val player1Name: String, val player2Name: String) extends Tenn
     else
       P2point += 1
   }
+
 }
